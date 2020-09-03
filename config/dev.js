229b0dbdf3f926252d00ea1cae7b03de428bc976
Bird Kim <pleasant-life@hanmail.net>
2020-09-03 17:35:50 +0900

    encrypt password

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://birdkim:tmax2020@boilerplate.z2irs.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5000
+//use key_20200903
+const config = require ('./config/key');
+
+
+
 //use post
 const bodyParser = require('body-parser');
 const { User } = require("./models/Users");
@@ -8,13 +13,23 @@ const { User } = require("./models/Users");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
+//use key_20200903
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://birdkim:tmax2020@boilerplate.z2irs.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
+
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb+srv://birdkim:tmax2020@boilerplate.z2irs.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+//     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+// }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World! dldididid')
+  res.send('Hello World!')
 })
 
 //use post
