9d118fcc6ea3cc7f4cac2e29ef4602c9ebace3de
Bird Kim <pleasant-life@hanmail.net>
2020-09-09 16:00:06 +0900

    bcrypt

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ app.post('/register', (req, res) => {
 
   const user = new User(req.body)
   user.save((err, userInfo) => {
-      if(err) return res.json({ success: false, err})
-      return res.status(200).json({
-          success: true
-      })
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+        success: true
+    })
   })
 })
 
--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,28 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next){
+    
+    //비밀번호 암호화 시킴
+    // 스키마가 넘어가기전 먼저 처리하는 부분
+    var user = this; //this는 위 전체 스키마를 의미
+    if(user.isModified('password')){  //비밀번호 변경될때만 암호화하게 설정
+        
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        }) 
+    } else {
+        next()
+    }
+})
+
 
 const User = mongoose.model('User', userSchema) // model name is User, put userSchema in the Model
 
